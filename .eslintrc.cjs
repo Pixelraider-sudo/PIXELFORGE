f3c9e16bf6695a207aa3eea54543192067256b7b
Pixelraider-sudo <bitcranker@gmail.com>
2026-06-09 22:42:28 +0300

    fix: remove duplicate ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,26 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node: true },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:security/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'coverage'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'security'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'security/detect-object-injection': 'warn',
-    'security/detect-non-literal-regexp': 'warn',
+    'no-eval': 'error',
   },
 }
